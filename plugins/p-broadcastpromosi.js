ff43cf97d420906ebdf036dd1b1189fc7ca39203
NIGHTWITCH1 <84660661+NIGHTWITCH1@users.noreply.github.com>
2022-09-10 23:28:53 +0700

    Update p-broadcastpromosi.js

--- plugins/p-broadcastpromosi.js
+++ plugins/p-broadcastpromosi.js
@@ -4,9 +4,9 @@ let handler  = async (m, { conn, text }) => {
   let chats = Object.keys(await conn.chats)
   conn.reply(m.chat, `_Mengirim pesan broadcast promosi ke ${chats.length} chat_`, m)
   for (let id of chats) {
-       let bcbg = 'https://telegra.ph/file/b2ae8a0e437e4252d5124.png'
+       let bcbg = 'https://media.discordapp.net/attachments/849188337943969802/1018180867459391629/62_821-1366-5209_20220910_222701.jpg'
        await conn.delay(1500)
-       await conn.send2ButtonImg(id, bcbg, `${p}𓊈𒆜𝘉𝘳𝘰𝘢𝘥𝘤𝘢𝘴𝘵 𝘗𝘳𝘰𝘮𝘰𝘵𝘪𝘰𝘯𒆜𓊉${p}\n\n` + text.trim(), wm, 'Menu', '.menu', 'Owner', '.owner', ftroli)
+       await conn.send2ButtonImg(id, bcbg, `${p}𝘉𝘳𝘰𝘢𝘥𝘤𝘢𝘴𝘵 𝘗𝘳𝘰𝘮𝘰𝘵𝘪𝘰𝘯${p}\n\n` + text.trim(), wm, 'Menu', '.menu', 'Owner', '.owner', ftroli)
      }
   m.reply('*Broadcast promosi selesai*')
 }
